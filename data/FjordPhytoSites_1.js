3184b6af20f286d5f2aeefe881583af3fad68795
martinamascioni <marmascioni@gmail.com>
2021-09-22 12:06:18 -0300

    Initial commit

---
+++ data/FjordPhytoSites_1.js
@@ -0,0 +1,19 @@
+var json_FjordPhytoSites_1 = {
+"type": "FeatureCollection",
+"name": "FjordPhytoSites_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "PREFFERED SITE NAME": "Cuverville Island", "Longtitude (S)": "-64�40,2'", "Latitude (W)": "-62�38,4'", "Years": 3.0 }, "geometry": { "type": "Point", "coordinates": [ -62.634444444444448, -64.667222222222222 ] } },
+{ "type": "Feature", "properties": { "PREFFERED SITE NAME": "Danco Island", "Longtitude (S)": "-64�45,03'", "Latitude (W)": "-62�36,19'", "Years": 3.0 }, "geometry": { "type": "Point", "coordinates": [ -62.605277777777779, -64.750833333333333 ] } },
+{ "type": "Feature", "properties": { "PREFFERED SITE NAME": "Wilhelmina Bay ", "Longtitude (S)": "-64�39,41'", "Latitude (W)": "-62� 08,14'", "Years": 2.0 }, "geometry": { "type": "Point", "coordinates": [ -62.137222222222221, -64.661388888888894 ] } },
+{ "type": "Feature", "properties": { "PREFFERED SITE NAME": "Enterprise Island", "Longtitude (S)": "-64�30,43'", "Latitude (W)": "-61�58,48'", "Years": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -61.98, -64.511944444444438 ] } },
+{ "type": "Feature", "properties": { "PREFFERED SITE NAME": "Neko Harbour", "Longtitude (S)": "-64�50,57'", "Latitude (W)": "-62�32,22'", "Years": 3.0 }, "geometry": { "type": "Point", "coordinates": [ -62.539444444444442, -64.849166666666662 ] } },
+{ "type": "Feature", "properties": { "PREFFERED SITE NAME": "Orne Harbour", "Longtitude (S)": "-64�38,27'", "Latitude (W)": "-62�32,21'", "Years": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -62.539166666666667, -64.640833333333333 ] } },
+{ "type": "Feature", "properties": { "PREFFERED SITE NAME": "Paradise Harbour", "Longtitude (S)": "-64�53,40'", "Latitude (W)": "-62�52,44'", "Years": 2.0 }, "geometry": { "type": "Point", "coordinates": [ -62.878888888888888, -64.894444444444446 ] } },
+{ "type": "Feature", "properties": { "PREFFERED SITE NAME": "Lallemand Fjord ", "Longtitude (S)": "-66�57,12'", "Latitude (W)": "-66�50,14'", "Years": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -66.837222222222223, -66.953333333333333 ] } },
+{ "type": "Feature", "properties": { "PREFFERED SITE NAME": "Hanusse Bay ", "Longtitude (S)": "-66�53,56'", "Latitude (W)": "-67�13,39'", "Years": 3.0 }, "geometry": { "type": "Point", "coordinates": [ -67.2275, -66.898888888888891 ] } },
+{ "type": "Feature", "properties": { "PREFFERED SITE NAME": "Dallman Bay", "Longtitude (S)": "-64�39,01'", "Latitude (W)": "-63�09,01'", "Years": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -63.150277777777781, -64.650277777777774 ] } },
+{ "type": "Feature", "properties": { "PREFFERED SITE NAME": "Marguerite Bay", "Longtitude (S)": "-68�11,2'", "Latitude (W)": "-67�00,4'", "Years": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -67.001111111111115, -68.183888888888887 ] } },
+{ "type": "Feature", "properties": { "PREFFERED SITE NAME": "Gerlache Strait", "Longtitude (S)": "-64�33'36''", "Latitude (W)": "-62�35'24''", "Years": 1.0 }, "geometry": { "type": "Point", "coordinates": [ -62.59, -64.56 ] } }
+]
+}
